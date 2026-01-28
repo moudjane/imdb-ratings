7e948bf117fb8712cea0b823957a8484b544543a
Mathis Oudjane <mathis.oudjane@gmail.com>
2026-01-29 00:43:30 +0100

    add docker-compose and backend dockerization

--- backend/main.go
+++ backend/main.go
@@ -1,18 +1,13 @@
 package main
 
 import (
-	"log"
-
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
 	"github.com/moudjane/imdb-ratings/backend/handlers"
 )
 
 func main() {
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatal("Error loading .env file")
-	}
+	_ = godotenv.Load()
 
 	r := gin.Default()
 
